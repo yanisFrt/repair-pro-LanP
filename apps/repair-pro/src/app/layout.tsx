43cf51180bfd8e95a44f04318354bb93ce2d4068
Koceila <kouceiladrk@gmail.com>
2025-11-15 12:43:59 +0100

    feat(layout): add NavBar and footer to root layout

--- apps/repair-pro/src/app/layout.tsx
+++ apps/repair-pro/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import DynamicFooter from "@/components/Footer";
+import { NavBar } from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+        <DynamicFooter />
+      </body>
     </html>
   );
 }
