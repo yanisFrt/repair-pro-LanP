ef03373148d3e075482b05667bbf6a84e4b51e2c
Koceila <kouceiladrk@gmail.com>
2025-12-01 10:05:56 +0100

    feat(analytics): add google tag manager integration for tracking

--- apps/repair-pro/src/app/layout.tsx
+++ apps/repair-pro/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import DynamicFooter from "@/components/Footer";
 import { NavBar } from "@/components/Navbar";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +20,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        {/* Google Tag Manager  */}
+        <Script>
+          {" "}
+          {`
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WC4SPBV4');
+  `}
+        </Script>
+        {/* End Google Tag Manager  */}
+      </head>
       <body className={inter.className}>
         <NavBar />
         {children}
         <DynamicFooter />
+
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WC4SPBV4"
+            height="0"
+            width="0"
+            style={"display:none;visibility:hidden" as any}
+          ></iframe>
+        </noscript>
       </body>
+
+      <GoogleAnalytics />
     </html>
   );
 }
